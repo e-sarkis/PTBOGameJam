9bd49c9ac7b4e10b5fe15200b1f6b71453b2dc58
Matthew Davey <matthew.davey@dotbunny.com>
2016-11-19 11:13:49 -0500

    Switch to a fontello based single icon file format, fixes to CSS on about page.

--- content/js/theme.js
+++ content/js/theme.js
@@ -33,9 +33,6 @@ Version: 		1.0
 			//Mobile Menu
 			jQuery('#jx-main-menu,#jx-main-menu-2').slicknav();
 
-			// ScrollTop
-			this.scrollTop();
-
 			// Animation
 			this.animation();
 
@@ -379,31 +376,6 @@ Version: 		1.0
 
 			//circle Progressbar
 			jQuery('.circliful').appear();
-
-		},
-
-		scrollTop: function(){
-
-			jQuery.scrollUp({
-						scrollName: 'scrollUp', // Element ID
-						scrollDistance: 300, // Distance from top/bottom before showing element (px)
-						scrollFrom: 'top', // 'top' or 'bottom'
-						scrollSpeed: 300, // Speed back to top (ms)
-						easingType: 'linear', // Scroll to top easing (see http://easings.net/)
-						animation: 'fade', // Fade, slide, none
-						animationInSpeed: 200, // Animation in speed (ms)
-						animationOutSpeed: 200, // Animation out speed (ms)
-						scrollTitle: false, // Set a custom <a> title if required. Defaults to scrollText
-						scrollImg: false, // Set true to use image
-						activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-						zIndex: 2147483647 // Z-Index for the overlay
-					});
-
-			jQuery(function($){
-				jQuery('.destroy').on("click",function($){
-					$.scrollUp.destroy();
-				})
-			});
 
 		},
 
