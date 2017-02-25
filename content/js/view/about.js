c4eb345a7d49da0bf7431571ac33728af715ee09
Matthew Davey <reapazor@gmail.com>
2017-02-25 13:51:21 -0500

    Working on statistics for the about page.

---
+++ content/js/view/about.js
@@ -0,0 +1,198 @@
+var chartAttendance = new Chart($("canvas#attendance-chart"), { 
+    type: 'line', 
+    data: {
+        labels: ["", "01", "02"],
+        datasets: [
+        {
+            label: "Attendees",
+            fill: false,
+            lineTension: 0.1,         
+            backgroundColor: "rgba(0,204,255,0.8)",
+            borderColor: "rgba(0,204,255,0.4)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(0,204,255,1)",
+            pointBackgroundColor: "rgba(0,204,255,1)",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            // pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            // pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [0,43, 155],
+            spanGaps: false,
+        },
+        {
+            label: "Jammers",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(255,102,0,0.8)",
+            borderColor: "rgba(255,102,0,0.4)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(255,102,0,1)",
+            pointBackgroundColor: "rgba(255,102,0,1)",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [0,17, 80],
+            spanGaps: false,
+        },
+        {
+            label: "No Shows",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(255,204,0,0.8)",
+            borderColor: "rgba(255,204,0,0.4)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(255,204,0,1)",
+            pointBackgroundColor: "rgba(255,204,0,1)",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [0,5, 41],
+            spanGaps: false,
+        }]
+    }, 
+    options: { 
+        title: {
+            display: true,
+            text: 'ATTENDANCE',
+            position: 'top',
+            fontFamily: 'Raleway',
+            fontStyle: 'bold',
+            fontSize: 26
+        },
+        defaultFontSize: '20',
+        legend: {
+            position: 'bottom'
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    fontFamily: 'Raleway',
+                    fontStyle: 'bold',
+                    fontSize: 26
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    fontSize: 16
+                }
+            }]
+        }
+    } 
+});
+
+
+
+
+var chartGames = new Chart($("canvas#games-chart"), { 
+    type: 'line', 
+    data: {
+        labels: ["", "01", "02"],
+        datasets: [
+        {
+            label: "Total",
+            fill: false,
+            lineTension: 0.1,         
+            backgroundColor: "rgba(0,204,255,0.8)",
+            borderColor: "rgba(0,204,255,0.4)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(0,204,255,1)",
+            pointBackgroundColor: "rgba(0,204,255,1)",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            // pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            // pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [0,11, 33],
+            spanGaps: false,
+        },
+        {
+            label: "Solo",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(255,102,0,0.8)",
+            borderColor: "rgba(255,102,0,0.4)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(255,102,0,1)",
+            pointBackgroundColor: "rgba(255,102,0,1)",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [0,8, 15],
+            spanGaps: false,
+        },
+        {
+            label: "Team",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(255,204,0,0.8)",
+            borderColor: "rgba(255,204,0,0.4)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(255,204,0,1)",
+            pointBackgroundColor: "rgba(255,204,0,1)",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [0,3, 18],
+            spanGaps: false,
+        }]
+    }, 
+    options: { 
+        title: {
+            display: true,
+            text: 'GAMES MADE',
+            position: 'top',
+            fontFamily: 'Raleway',
+            fontStyle: 'bold',
+            fontSize: 26
+        },
+        defaultFontSize: '20',
+        legend: {
+            position: 'bottom'
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    fontFamily: 'Raleway',
+                    fontStyle: 'bold',
+                    fontSize: 26
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    fontSize: 16
+                }
+            }]
+        }
+    } 
+});
