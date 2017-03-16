b0784bf52de6e25ecf1cbab7ed9552bddfddfd33
Matthew Davey <reapazor@gmail.com>
2017-03-16 19:55:23 -0400

    Initial AJAX callback

---
+++ content/js/view/archive.js
@@ -0,0 +1,22 @@
+// Data Process
+var jqxhr = $.getJSON( "/data/archive.json", function(data) {
+  
+    console.log(data);
+  
+})
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
+ 
+// Perform other work here ...
+ 
+// Set another completion function for the request above
+jqxhr.complete(function() {
+  console.log( "second complete" );
+});
